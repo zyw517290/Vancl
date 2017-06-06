b2cc6f33780279a59546773336af3a71b7865efb
zyw517290 <2523929851@qq.com>
2017-06-06 16:23:54 +0800

    功能的补充

--- src/js/page/kind.js
+++ src/js/page/kind.js
@@ -1,6 +1,12 @@
 var sortPage = {
 	sortInit:function(){
 		this.sortTab();
+		this.sortMessage();
+		this.sortHref();
+		this.sortHrefReturn();
+		this.sortPicChange();
+		this.verticalTab();
+		this.listChange();
 	},
 //	tab切换
 	sortTab:function(){
@@ -9,6 +15,47 @@ var sortPage = {
 			
 			$(this).addClass("van-sort-content-picture-nav-active").show().siblings().removeClass("van-sort-content-picture-nav-active");
 			$(".van-sort-content-picture-wrap .van-sort-content-picture-list").eq($(this).index()).show().siblings().hide();
+		})
+	},
+//	详细信息页面tab切换
+	sortMessage:function(){
+		$(".vancl-sort-message-main-wrap .sort-message-main-wrap").eq(0).show().siblings().hide();
+		$(".sort-message-main-nav-tab").on("click",function(){
+			$(this).addClass("sort-message-main-nav-active").siblings().removeClass("sort-message-main-nav-active");
+			$(".vancl-sort-message-main-wrap .sort-message-main-wrap").eq($(this).index()).show().siblings().hide();
+		})
+	},
+//	location跳转
+	sortHref:function(){
+		$(".js-van-sort-content-picture-list-groom").on("click",function(){
+			window.location.href = "sortMessage.html";
+		})
+	},
+	sortHrefReturn:function(){
+		$(".js-message-header-return").on("click",function(){
+			window.location.href = "kind.html";
+		})		
+	},
+//	头部图片变换
+	sortPicChange:function(){
+		$("#js-message-header-change").on("click",function(){
+			$(this).toggleClass('message-header-change1');			
+		})
+	},
+	verticalTab:function(){
+		$(".js-vancl-sort-message-vertical .js-sort-message-vertical").eq(0).show().siblings().hide();
+		$(".sort-message-main-nav-tab").on("click",function(){
+			$(this).addClass('sort-message-main-nav-active').siblings().removeClass('sort-message-main-nav-active');
+			$(".js-vancl-sort-message-vertical .js-sort-message-vertical").eq($(this).index()).show().siblings().hide();
+		})		
+	},
+//	点击头部图片时变换详细信息格式
+	listChange:function(){
+		$("#js-message-header-change").on("click",function(){
+			$('.js-vancl-sort-message-vertical').show();
+		});
+		$("#js-message-header-change").on("click",function(){
+			$('.vancl-sort-message-main-wrap').hide();
 		})
 	}
 }
